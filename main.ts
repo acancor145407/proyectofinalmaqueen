0a792054edceda471d3f7a3f146743fa59ad9c08
acancor145407 <98743495+acancor145407@users.noreply.github.com>
2022-04-17 21:15:57 +0200

    proyectoFinalMaqueenTerminado

--- main.ts
+++ main.ts
@@ -31,6 +31,31 @@ radio.onReceivedNumber(function (receivedNumber) {
             # . . . #
             `)
     }
+    if (receivedNumber == 4) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) == 0 && maqueen.Ultrasonic(PingUnit.Centimeters) == 0) {
+            maqueen.motorStop(maqueen.Motors.M1)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 25)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+            basic.pause(500)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 25)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 25)
+        }
+    }
+    if (receivedNumber == 5) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
+    if (receivedNumber == 6) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
 })
 radio.setGroup(14)
 basic.showIcon(IconNames.Happy)
